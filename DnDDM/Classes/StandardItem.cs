eccc3e1a7f584dc1e077d55321363f1aabf35573
Xiokraze <xiokraze@gmail.com>
2020-12-31 12:57:31 -0600

    Added standard items class.

---
+++ DnDDM/Classes/StandardItem.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDDM.Classes
+{
+    public class StandardItem
+    {
+        public string ArmorClass { get; set; }
+        public string Name { get; set; }
+        public string Classification { get; set; }
+        public string Damage { get; set; }
+        public string Description { get; set; }
+        public string Properties { get; set; }
+        public string RequiredStrength { get; set; }
+        public string StealthImpact { get; set; }
+        public string Value { get; set; }
+        public string Weight { get; set; }
+
+
+        // Armor Consctructor.
+        public StandardItem(string description, string value, string armorClass, string requiredStrength, string stealthImpact, string weight, string classification)
+        {
+            this.Description = description;
+            this.Value = value;
+            this.ArmorClass = armorClass;
+            this.RequiredStrength = requiredStrength;
+            this.StealthImpact = stealthImpact;
+            this.Weight = weight;
+            this.Classification = classification;
+        }
+
+
+        // Art Constructor.
+        public StandardItem(string description, string value)
+        {
+            this.Description = description;
+            this.Value = value;
+            this.Classification = DefaultValues.Art;
+        }
+
+
+        // Gem Constructor.
+        public StandardItem(string name, string description, string value)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Value = value;
+            this.Classification = DefaultValues.Gem;
+        }
+
+
+        // Good Constructor.
+        public StandardItem(string description, string value, string weight, string classification)
+        {
+            this.Description = description;
+            this.Value = value;
+            this.Weight = weight;
+            this.Classification = classification;
+        }
+
+
+        // Trinket Constructor.
+        public StandardItem(string description)
+        {
+            this.Description = description;
+            this.Classification = DefaultValues.Trinket;
+        }
+
+
+        // Weapon Constructor.
+        public StandardItem(string description, string value, string damage, string weight, string properties, string classification)
+        {
+            this.Description = description;
+            this.Value = value;
+            this.Damage = damage;
+            this.Weight = weight;
+            this.Properties = properties;
+            this.Classification = classification;
+        }
+    }
+}
