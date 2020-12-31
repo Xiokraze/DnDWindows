712510bffff21be8a589ad76e36a01b8dec5198d
Xiokraze <xiokraze@gmail.com>
2020-12-31 12:56:02 -0600

    Program: Added method for adding control to the main form and removing extras.

---
+++ DnDDM/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DnDDM
+{
+    static class Program
+    {
+        // Global Variables.
+        public static Control.ControlCollection controlCollection;
+        public static MainForm mainForm;
+        public static MenuStrip mainMenu;
+
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+
+
+        /// <summary>
+        /// Removes all controls save for the new control and the header panel.
+        /// </summary>
+        /// <param name="newControl"></param>
+        public static void AddControl(Control newControl)
+        {
+            controlCollection.Add(newControl);
+            foreach (Control control in controlCollection)
+            {
+                if (!object.Equals(control, newControl) && control.Name != "menuStrip")
+                {
+                    controlCollection.Remove(control);
+                }
+            }
+        }
+    }
+}
