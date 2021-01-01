531a337426b06ea3ca1f1241b83e683b9d68c745
Xiokraze <xiokraze@gmail.com>
2020-12-31 21:07:51 -0600

    Added standard item control.

---
+++ DnDDM/Controls/StandardItemControl.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace DnDDM.Controls
+{
+    partial class StandardItemControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(800, 328);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.dataGridView);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 100);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 328);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 100);
+            this.panel2.TabIndex = 2;
+            // 
+            // StandardItemControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(3)))), ((int)(((byte)(3)))));
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(206)))), ((int)(((byte)(101)))));
+            this.Name = "StandardItemControl";
+            this.Size = new System.Drawing.Size(800, 600);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
---
+++ DnDDM/Controls/StandardItemControl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DnDDM.Classes;
+
+namespace DnDDM.Controls
+{
+    public partial class StandardItemControl : UserControl
+    {
+        private readonly StandardItemLists itemLists = new StandardItemLists();
+        public StandardItemControl(string control)
+        {
+            InitializeComponent();
+            UpdateGUI(control);
+        }
+
+
+        private void UpdateGUI(string control)
+        {
+            if (control.Equals(DefaultValues.Armor))
+            {
+                UpdateArmorTable();
+            }
+        }
+
+
+        private void UpdateArmorTable()
+        {
+            dataGridView.Columns.Add("Classification", "Classification");
+            dataGridView.Columns.Add("Description", "Description");
+            dataGridView.Columns.Add("Value", "Value");
+            dataGridView.Columns.Add("ArmorClass", "AC");
+            dataGridView.Columns.Add("RequiredStrength", "Strength");
+            dataGridView.Columns.Add("StealthImpact", "Stealth");
+            dataGridView.Columns.Add("Weight", "Weight");
+            dataGridView.DataSource = itemLists.armor;
+        }
+    }
+}
